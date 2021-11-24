3eca27165b2112c77b4620cfadf9f28e7857b039
Chi heon Song <hrpp1353@gmail.com>
2021-11-24 20:17:05 +0900

    feat: Tire list View
    - 유저가 소유한 자동차의 타이어 리스트를 확인하는 로직 작성성

--- cars/urls.py
+++ cars/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from cars.views import TireCreateView
 
 urlpatterns = [
-    path('/tire', TireCreateView.as_view())
+    path('/tire', TireCreateView.as_view()),
+    path('/users/<str:user_id>', TireCreateView.as_view())
 ]
